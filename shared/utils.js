3856597b668281b6f286ddfdc77add759e262095
Mario Linares <mario.linares@vass.es>
2023-10-18 23:07:02 +0200

    ds

--- shared/utils.js
+++ shared/utils.js
@@ -27,9 +27,9 @@ class Reticle extends THREE.Object3D {
 
     //this.loader = new THREE.GLTFLoader();
     this.loader = new THREE.OBJLoader();
-    this.loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", (obs) => {
+/*     this.loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", (obs) => {
       this.add(obs.scene);
-    })
+    }) */
 
     this.visible = false;
   }
